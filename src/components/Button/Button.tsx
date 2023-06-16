3a885733cb45ea5d0b0a0f97e8face6f84d525a3
Fernando G <fernanprojects@gmail.com>
2023-06-16 18:43:21 +0200

    added scss architecture

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../styles/theme/05.components/atoms/_button.scss";
 
 type ButtonProps = {
   label: string;
 };
 
 export const Button = ({ label }: ButtonProps) => {
-  return <button >{label}</button>;
+  return <button className="btn btn-primary" >{label}</button>;
 };
