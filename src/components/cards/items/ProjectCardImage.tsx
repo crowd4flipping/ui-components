7d0a66206503e9edd86aa1e8cd0f9d1a8832ae46
Fernando G <fernando@crowd4flipping.com>
2023-07-11 13:58:00 +0200

    Removed ReactElement type from ProjectCard src property

--- src/components/cards/items/ProjectCardImage.tsx
+++ src/components/cards/items/ProjectCardImage.tsx
@@ -1,29 +1,16 @@
-import React, { ReactElement } from "react";
+import React from "react";
 
 type ProjectCardImageProps = {
-  src: string | ReactElement;
+  src: string;
 };
 
 export const ProjectCardImage = ({ src }: ProjectCardImageProps) => {
   return (
-    <>
-      {isReactElement(src) ? (
-        { src }
-      ) : (
-        <img
-          loading="lazy"
-          alt="imagen del proyecto"
-          src={src}
-          className="project-card__image"
-        />
-      )}
-    </>
+    <img
+      loading="lazy"
+      alt="imagen del proyecto"
+      src={src}
+      className="project-card__image"
+    />
   );
-
-  function isReactElement(value: unknown): value is ReactElement {
-    const obj = Object.keys(value as object);
-    const type = obj.find((prop) => prop == "type");
-    if (!type) return false;
-    return true;
-  }
 };
