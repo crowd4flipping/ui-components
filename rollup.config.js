ce254fda56cfe8110cf46c93b536ddd1cc02cae5
Fernando G <fernando@crowd4flipping.com>
2023-11-27 19:19:14 +0100

    Remove rollup extract css modules

--- rollup.config.js
+++ rollup.config.js
@@ -25,8 +25,6 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss({
-        extract: true,
-        modules: true,
         plugins: [],
       }),
     ],
