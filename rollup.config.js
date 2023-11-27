34a1f57ac2325d0e93d9fa1d41bf6e3ba48240e8
Fernando G <fernando@crowd4flipping.com>
2023-11-27 20:07:17 +0100

    Remove rollup css plugins

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,6 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 //import postcssJitProps from "postcss-jit-props";
-import css from "rollup-plugin-css-only";
-import terser from "@rollup/plugin-terser";
-import scss from "rollup-plugin-scss";
 
 export default [
   {
@@ -27,15 +24,8 @@ export default [
       }),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-      scss({
-        include: [
-          "./src/components/styles/theme/02.generics/_all-generics.scss",
-        ],
-      }),
       postcss({
-        modules: true,
-        extract: true,
+        plugins: [],
       }),
     ],
     external: ["react", "react-dom", "next/image"],
